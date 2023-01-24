51a0ac6b4a2e9bf6814c90c565230108f911385b
Liam Zimmermann <zimmerml@oregonstate.edu>
2023-01-23 22:56:28 -0800

    change replace into a function that searches out the variables

--- lambda.hs
+++ lambda.hs
@@ -1,14 +1,10 @@
 import Data.Maybe
 import qualified Data.Text as Text
 
---------------------------------------------------------------------------------
--- HELPY
---------------------------------------------------------------------------------
 
-
---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
 -- SYNTAX
---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
 
 data Expr = Var VarName
           | App Expr Expr
@@ -19,13 +15,13 @@ type VarName = String
 
 pp :: Expr -> String
 pp (Var vn) = vn
-pp (App e1 e2) = (pp e1) ++ "(" ++ (pp e2) ++ ")"
+pp (App e1 e2) = "(" ++ (pp e1) ++ " " ++ (pp e2) ++ ")"
 pp (Abstr vn e) = "\\" ++ vn ++ "." ++ (pp e)
 
 
---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
 -- SEMANTICS
---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
 
 bReduc :: Expr -> Expr
 bReduc (Var vn) = (Var vn)
@@ -33,17 +29,34 @@ bReduc (Abstr vn e) = (Abstr vn (bReduc e))
 bReduc (App e1 e2) = bApp (bReduc e1) (bReduc e2)
 
 bApp :: Expr -> Expr -> Expr
-bApp (Abstr vn (Var vb)) (Var vo) = Var (Text.unpack (
-                                        Text.replace
-                                        (Text.pack vn)
-                                        (Text.pack vo)
-                                        (Text.pack vb)
-                                        ))
-bApp e1 e2 = App e1 e2
+bApp (Abstr vn e1) e2 = let e2' = bReduc e2 in
+                            let e1' = replaceVar vn e2' e1 in
+                                bReduc e1'
+bApp e1 e2 = App (bReduc e1) (bReduc e2)
 
+--            needle     repl    haystack
+replaceVar :: VarName -> Expr -> Expr -> Expr
+replaceVar ndl repl (Var hstk) = if ndl == hstk then repl else Var hstk
+replaceVar ndl repl (App hstk1 hstk2) = App (replaceVar ndl repl hstk1) 
+                                            (replaceVar ndl repl hstk2)
+replaceVar ndl repl (Abstr hstk_vn hstk_e) = Abstr hstk_vn 
+                                                   (replaceVar ndl repl hstk_e)
+
+{-
+
+EXAMPLE I FAILED ON?
+
+
+(\x.\y.(y x) (y w))         
+\z.(z (y w))        
+"Avoid capturing the free variable y in (y w)"
+-}
 
 
 main :: IO ()
-main = putStrLn (pp (Abstr "x" (Var "x^2")))
+main = do
+    let expr    = App (Abstr "x" (Abstr "y" (App (Var "y") (Var "x")))) (App (Var "y") (Var "w"))
+    let expr'   = bReduc expr
+    putStrLn $ pp expr
+    putStrLn $ pp expr'
 
--- Need to make semantics.
