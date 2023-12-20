dccbe901274c82e7c0c8032876fb908f91271fc9
zimmermannliam <zimmerml@oregonstate.edu>
2023-12-19 16:28:39 -0800

    Add more ellipsis

--- lambda.hs
+++ lambda.hs
@@ -11,22 +11,30 @@ data Expr = Var Name
           | Value Val
           | Let Name Expr Expr
           | Case Expr Alts
-          | LetRec Name Expr Expr
+          | LetRec Name Expr Expr -- LetRec "Name" = Expr in Expr
           | Fix Expr
           | Add Expr Expr
           | Tail Expr
-          | EllipsisE Expr Expr Expr -- For y1 ... yn: EllipsesE y 1 n
+          | EllipsisE Expr Idx Idx -- For y1 ... yn: EllipsesE y 1 n
           | ConsE Expr Expr
+          | Cat Expr Expr
+          | Error String
+          | Report Expr
+          | EPair Expr Expr
+          | EStr String
           deriving (Eq, Show)
 
 data Val = Con Int 
-          | Cons Int Val 
+          | ICons Int Val 
+          | VCons Val Val
           | Empty 
           | Closure Name Expr Env
           | Fun Name Expr
           | Plus Val Val
           | Fixed Expr
           | FreeVar Name
+          | VPair Val Val
+          | VStr String
          deriving (Eq, Show)
 
 data Pattern = PCons Name Name
@@ -35,7 +43,9 @@ data Pattern = PCons Name Name
              | PEllipsis Name Idx
              deriving (Eq, Show)
 
-data Idx = Place Int | End Name
+data Idx    = IPlace Int 
+            | End Name 
+            | EPlace Expr -- Must evaluate to an integer
             deriving (Eq, Show)
 
 type Name = String
@@ -63,7 +73,7 @@ eval :: Env -> Expr -> Val
 eval e (Var vn)            = envLookup e vn
 eval e (App t1 t2)         = case eval e t1 of 
                                 Closure x t1b e2 -> eval ((x, eval e t2):e2) t1b
-                                _           -> error "Expected fn to be applied"
+                                _           -> errorOut e "Expected fn to be applied"
 eval e (Abstr x t)         = Closure x t e
 eval e (Let n t1 t2)       = eval ((n, eval e t1):e) t2
 eval e (Add t1 t2)         = case (eval e t1, eval e t2) of
@@ -72,14 +82,34 @@ eval e (Add t1 t2)         = case (eval e t1, eval e t2) of
 eval e (LetRec n t1 t2)    = case eval e t1 of
                                 Closure {}  -> eval ((n, eval e $ App ycomb (Abstr n t1)):e) t2
                                                     -- fn = fix (\fn.t1)
-                                _           -> error "Expected fn to be letrecced"
+                                _           -> errorOut e "Expected fn to be letrecced"
 eval e (Value v)            = v
 eval e (Tail t)             = case eval e t of
-                                (Cons _ tail)           -> tail
-                                _                       -> error ("not a list " ++ show t)
+                                (ICons _ tail)           -> tail
+                                _                       -> errorOut e ("not a list " ++ show t)
 eval e (Case tc ps)   = patternMatchEval e tc ps
-eval e (ConsE t1 t2)  = Cons (valEvalInt $ eval e t1) (eval e t2)
-eval e _              = error "Feature not implemented"
+eval e (ConsE t1 t2)  = VCons (eval e t1) (eval e t2)
+eval e (EPair t1 t2)  = VPair (eval e t1) (eval e t2)
+eval e (Cat t1 t2)    = let et1 = iconsToVCons $ eval e t1
+                            et2 = iconsToVCons $ eval e t2
+                        in case (et1, et2) of
+                            (VCons _ _, VCons _ _) -> catVCons et1 et2
+                            _                      -> errorOut e "Tried to cat non-lists"
+eval e (Error s)      = errorOut e s
+eval e _              = errorOut e "Feature not implemented"
+
+errorOut :: Env -> String -> Val
+errorOut e s = error (s ++ "; Environment: " ++ ppEnv e)
+
+iconsToVCons :: Val -> Val
+iconsToVCons (ICons x xs)   = VCons (Con x) (iconsToVCons xs)
+iconsToVCons x              = x
+
+catVCons :: Val -> Val -> Val
+catVCons (VCons x xs) ys    = VCons x (catVCons xs ys)
+catVCons Empty ys           = ys
+catVCons _ _                = error "Tried to catVCons non-lists"
+
 
 valEvalInt :: Val -> Int
 valEvalInt (Con t)  = t
@@ -97,11 +127,12 @@ patternMatch :: Env -> Expr -> (Pattern, Expr) -> Maybe Val
 patternMatch e t (PVal k, t2)             = if eval e t == k then Just $ eval e t2
                                                 else Nothing
 patternMatch e t (PCons n ns, t2)         = case eval e t of
-                                                Cons x xs   -> Just $ eval ((n,Con x):(ns,xs):e) t2
+                                                ICons x xs  -> Just $ eval ((n,Con x):(ns,xs):e) t2
+                                                VCons x xs  -> Just $ eval ((n, x):(ns,xs):e) t2
                                                 _           -> Nothing
 patternMatch e t (PVar n, t2)             = Just $ eval ((n, eval e t):e) t2 
 patternMatch e t (PEllipsis n len, t2)    = case eval e t of
-                                                Cons x xs   -> Nothing
+                                                ICons x xs   -> Nothing
                                                 _           -> Nothing
 -- patternMatch e t _                          = Nothing
 
@@ -122,10 +153,12 @@ pp (Let n e1 e2) = "Let " ++ n ++ " = (" ++ pp e1 ++ ") in " ++ pp e2
 pp (Case e a)   = "Case " ++ pp e ++ " of {" ++ ppMatch a ++ "\n}"
 pp (LetRec  n e1 e2)    = "Letrec " ++ n ++ " = (" ++ pp e1 ++ ")\nin " ++ pp e2
 pp (Add e1 e2)  = "(" ++ pp e1 ++ " + " ++ pp e2 ++ ")"
-pp (ConsE e1 e2) = "(" ++ pp e1 ++ " ++ " ++ pp e2 ++ ")"
-pp (EllipsisE n start end)  = strn ++ pp start ++ " ... " ++ strn ++ pp end
+pp (ConsE e1 e2) = "(" ++ pp e1 ++ " : " ++ pp e2 ++ ")"
+pp (EllipsisE n start end)  = strn ++ ppIdx start ++ " ... " ++ strn ++ ppIdx end
                                 where strn = pp n
-pp _            = "Error"
+pp (EPair t1 t2) = "(" ++ pp t1 ++ ", " ++ pp t2 ++ ")"
+pp (Cat t1 t2) = pp t1 ++ " ++ " ++ pp t2
+pp _            = "Error -- cannot display expression"
 
 ppMatch :: Alts -> String
 ppMatch []              = ""
@@ -139,24 +172,33 @@ ppPattern (PEllipsis n i) = strn ++ "1 ... " ++ strn ++ ppIdx i
                         where strn = n
 
 ppIdx :: Idx -> String
-ppIdx (Place i)   = show i
+ppIdx (IPlace i)   = show i
 ppIdx (End n)     = n
+ppIdx (EPlace t) = pp t
 
 ppVal :: Val -> String
 ppVal (Con i)       = show i
-ppVal (Cons i v)    = show i ++ " " ++ ppVal v
+ppVal (ICons i v)    = show i ++ " " ++ ppVal v
+ppVal (VCons v vs)   = ppVal v ++ " " ++ ppVal vs
 ppVal Empty       = "Empty"
 ppVal (Closure n e _)   = "Function " ++ n ++ " : " ++ pp e
 ppVal (Plus v1 v2)  = ppVal v1 ++ " + " ++ ppVal v2
 ppVal (FreeVar n)   = n
+ppVal (VPair v1 v2) = "(" ++ ppVal v1 ++ ", " ++ ppVal v2 ++ ")"
 ppVal _             = "Error"
+
+ppEnv :: Env -> String
+ppEnv e     = "<\n" ++ ppEnv' e ++ ">"
+    where
+    ppEnv' ((n,v):es)   = n ++ " <- " ++ ppVal v ++ "\n"
+    ppEnv' []           = ""
 
 ------------------------------------------------------------------------
 -- EXAMPLES
 ------------------------------------------------------------------------
 
-exList = Cons 1 $ Cons 2 $ Cons 3 $ Cons 4 $ Cons 5 Empty
-exList2 = Cons 8 $ Cons 14 $ Cons 32 $ Cons 0 $ Cons 4 Empty
+exList = ICons 1 $ ICons 2 $ ICons 3 $ ICons 4 $ ICons 5 Empty
+exList2 = ICons 8 $ ICons 14 $ ICons 32 $ ICons 0 $ ICons 4 Empty
 
 succ' :: Expr
 succ' = Abstr "x" $ Add (Var "x") (Value $ Con 1)
@@ -167,9 +209,52 @@ head' = Abstr "l" $ Case (Var "l") [(PVal Empty, Value Empty), (PCons "H" "T", V
 tail' :: Expr
 tail' = Abstr "l" $ Case (Var "l") [(PVal Empty, Value Empty), (PCons "H" "T", Var "T")]
 
+tail2' :: Expr
+tail2' = Abstr "l" $ Case (Var "l") [(PEllipsis "x" (End "n"), EllipsisE (Var "x") (IPlace 2) (End "n"))]
+
+removeNth' :: Expr
+removeNth' = Abstr "l" $ Abstr "i" $ 
+            LetRec "removeNth" (Abstr "list" $ Abstr "idx" $ Case (Var "idx")
+            [
+                (PVal $ Con 0, Case (Var "list")
+                [
+                    (PCons "x" "xs", Var "xs"),
+                    (PVal Empty, Error "zero idx out of bounds"),
+                    (PVar "_", Error "zero idx match error!")
+                ]),
+                (PVar "idx'", Case (Var "list")
+                [
+                    (PCons "x" "xs", ConsE (Var "x") 
+                                           (App (App (Var "removeNth") 
+                                               (Var "xs")) 
+                                               (Add (Var "idx'") (Value $ Con (-1)))
+                                           )
+                    ),
+                    (PVal Empty, Error "nonzero idx out of bounds"),
+                    (PVar "_", Error "nonzero idx match error!")
+                ])
+            ])
+            -- in
+            (App (App (Var "removeNth") (Var "l")) (Var "i"))
+
+removeNth2' :: Expr
+removeNth2' = Abstr "l" $ Abstr "n" $ 
+        Case (Var "l") -- of
+        [
+            (PEllipsis "x" (End "m"), Cat 
+                (EllipsisE (Var "x") (IPlace 1) (EPlace $ Add (Var "n") (Value $ Con (-1))))
+                (EllipsisE (Var "x") (EPlace $ Add (Var "n") (Value $ Con 1)) (End "m"))
+                )
+        ]
+
 add' :: Expr
 add' = Abstr "a" $ Abstr "b" $ Add (Var "a") (Var "b")
 
+map2' :: Expr
+map2' = Abstr "l" $ Abstr "f" $ Case (Var "l") -- of
+        [
+            (PEllipsis "x" (End "n"), EllipsisE (App (Var "f") (Var "x")) (IPlace 1) (End "n"))
+        ]
 
 map' :: Expr
 map' = Abstr "l" $ Abstr "f" $
@@ -317,6 +402,6 @@ test =
         print $ testCase (App (App fst' (Value $ Con 1)) (Value $ Con 2)) (Con 1)
         print $ testCase (App (App snd' (Value $ Con 1)) (Value $ Con 2)) (Con 2)
         print $ testCase (App (App nth' (Value exList)) (Value $ Con 2)) (Con 3)
-        print $ testCase (App sucEach' (Value exList)) (Cons 2 $ Cons 3 $ Cons 4 $ Cons 5 $ Cons 6 Empty)
+        print $ testCase (App sucEach' (Value exList)) (ICons 2 $ ICons 3 $ ICons 4 $ ICons 5 $ ICons 6 Empty)
         print $ testCase (App id' (Value exList)) exList
-        print $ testCase (App (App map' (Value exList2)) succ') (Cons 9 $ Cons 15 $ Cons 33 $ Cons 1 $ Cons 5 Empty)
+        print $ testCase (App (App map' (Value exList2)) succ') (ICons 9 $ ICons 15 $ ICons 33 $ ICons 1 $ ICons 5 Empty)
