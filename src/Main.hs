8efce925aead1067b07abebddb86c2e73756a34c
Liam Zimmermann <zimmerml@oregonstate.edu>
2024-04-16 13:01:05 -0700

    Reorder main

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,9 @@
 module Main where
 
 import EllipLang.Examples
-import EllipLang.Pretty ( pp )
+import EllipLang.Pretty ( pp, ppVal )
 import EllipLang.Syntax
+import EllipLang.Eval
 
 favorites :: [(String, Expr)]
 favorites = [("binSearch", binSearch'), ("pairAdj", pairAdj'), ("rotL", rotL')]
@@ -12,4 +13,4 @@ printFavorites :: IO ()
 printFavorites = putStrLn $ foldl (\x y -> x++"\n\n"++y) "\nFavorite examples:" $ map (\(n, e) -> n ++ ": \n" ++ pp e) favorites
 
 main :: IO ()
-main = printFavorites
+main = putStrLn $ ppVal $ eval prelude $ combinations `App` exList `App` exList2
