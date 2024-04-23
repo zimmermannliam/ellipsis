b9b6f31e7e1b85c9951ce64f8b30064b70d061c7
Liam Zimmermann <zimmerml@oregonstate.edu>
2024-04-23 15:29:09 -0700

    add smartcons to support bigger prelude

--- src/EllipLang/SmartCons.hs
+++ src/EllipLang/SmartCons.hs
@@ -5,7 +5,7 @@ import Data.Function ((&))
 
 import EllipLang.Syntax
 
-[f,x,n,l,r,m,k,t,y,k',xs,ys,list,term,g,a,b,list1,list2,l1,l2] = map Var ["f","x","n","l","r","m","k","t","y","k'","xs","ys","list","term","g","a","b","list1","list2","l1","l2"]
+[f,x,n,l,r,m,k,t,y,k',xs,ys,list,term,g,a,b,list1,list2,l1,l2,begin,end] = map Var ["f","x","n","l","r","m","k","t","y","k'","xs","ys","list","term","g","a","b","list1","list2","l1","l2","begin","end"]
 [x0,x1,x2] = map EllipVar [0,1,2]
 
 true = Value $ Boolean True
