cc0bd318c1c0125e95b92dc3183932f0109e99d0
Liam Zimmermann <zimmerml@oregonstate.edu>
2024-04-29 19:25:37 -0700

    add gzipM

--- src/GenericHelper.hs
+++ src/GenericHelper.hs
@@ -13,8 +13,8 @@ import Data.Generics
 import Control.Monad.State
 
 everywhereUntil :: GenericQ Bool -> GenericT -> GenericT
--- Variation on everywhere that executes f on the last thing to 
--- trigger q, but does not traverse inside.
+-- Variation on everywhere:
+-- Cease traversal if q holds for x, but run f x.
 everywhereUntil q f = go
     where
     go :: GenericT
@@ -34,3 +34,36 @@ mkTTMaybe f x y = case (cast x, cast y) of
 
 runStateEverywhere :: (Data a, Data b) => (b -> State s b) -> s -> a -> (a, s)
 runStateEverywhere f initState t = runState (everywhereM (mkM f) t) initState
+
+gzipM :: (Monad m, Typeable m) 
+    => (forall a. Data a => a -> (forall b. Data b => b -> Maybe (m b)))
+    -> (forall c. Data c => c -> (forall d. Data d => d -> Maybe (m d)))
+gzipM f x y = Just (gzipM' f x y)
+    where
+    gzipM' :: (Monad m, Typeable m) 
+        => (forall a. Data a => a -> (forall b. Data b => b -> Maybe (m b)))
+        -> (forall c. Data c => c -> (forall d. Data d => d -> m d))
+    gzipM' f x y = case (f x y) of
+        Just res    -> res
+        Nothing     -> if toConstr x == toConstr y
+            then gzipWithM (gzipM' f) x y
+            else error "gzipM: Structure mismatch"
+
+
+mkMM :: (Monad m, Typeable m, Data a)
+    => (a -> a -> m a)
+    -> (forall b. Data b => b -> (forall c. Data c => c -> Maybe (m c)))
+mkMM f x y = 
+    case (cast x, cast y) of
+        (Just (x' :: a), Just (y' :: a)) -> cast (f x' y')
+        _                                -> Nothing
+
+mkMMMaybe :: (Monad m, Typeable m, Data a)
+    => (a -> a -> Maybe (m a))
+    -> (forall b. Data b => b -> (forall c. Data c => c -> Maybe (m c)))
+mkMMMaybe f x y =
+    case (cast x, cast y) of
+        (Just (x' :: a), Just (y' :: a)) -> case (f x' y') of
+            Just res -> cast res
+            Nothing  -> Nothing
+        _                                -> Nothing
